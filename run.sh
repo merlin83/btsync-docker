f338e84ffa2a730d1ae3a4fec107e315f820ddbd
rubik <rubik@dev>
2014-10-10 21:28:10 +0800

    Allow Secret to be specified as an argument to run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,18 @@ if [ ! -d /opt/btsync ]; then
     mkdir -p /opt/btsync
 fi;
 
+if [ ! -e conf/btsync.conf ]; then
+    if [ -n $1 ]; then
+        echo "Using Secret: " $1;
+        sed 's/"secret" : ""/"secret" : "'$1'"/g' conf/btsync.conf.template > conf/btsync.conf
+    else
+        echo "No secret specified!";
+        cp conf/btsync.conf.template conf/btsync.conf
+    fi;
+else
+    echo "Using conf/btsync.conf";
+fi;
+
 docker run  -d -P \
             -v "/opt/btsync/:/btsync/" \
             -v "$PWD/conf/:/tmp/conf/" \
